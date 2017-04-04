6ea8a970c23250c135862d753047d3e601020e47
Dominic Brown and Grady Shelton <grady@>
2017-04-04 10:28:48 -0700

    add objects and a prototype that function properly, storing users form input

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,18 @@
 // BACK END
 
+function User(name, age) {
+  this.name = name;
+  this.age = age;
+}
 
+function Ticket(title, time) {
+  this.title = title;
+  this.time = time;
+}
 
+Ticket.prototype.price = function(age) {
+  console.log("user's age: " + age)
+}
 
 // FRONT END
 $(document).ready(function() {
@@ -10,14 +21,17 @@ $(document).ready(function() {
 
     // Gather user input from form fields
     var userName = $("#userName").val();
-    var userMovie = $("#movie-titles").val();
+    var userTitle = $("#movie-titles").val();
     var userShowTime = $("#show-times").val();
     var userAge = $("#age").val();
 
-    // var newUser =
+    var newUser = new User(userName,userAge);
+    var newTicket = new Ticket(userTitle,userShowTime);
+
+    newTicket.price(newUser.age);
 
     $(".ticket-summary #summary-sentence").text(userName);
-    $(".ticket-summary #movie-result").text(userMovie);
+    $(".ticket-summary #movie-result").text(userTitle);
     $(".ticket-summary #show-time-result").text(userShowTime);
     $(".ticket-summary #price-result").text("price");
 
